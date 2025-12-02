207476871095c256c275c1d24aaa2c6267a22f1b
THEBIGTOMMY13DEV <thebigtommy13tech@gmail.com>
2025-12-02 11:29:27 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const images = [
+    "./Images/ItalianFood1.jpg",
+    "./Images/ItalianFood2.jpg",
+    "./Images/GreekFood3.jpg",
+    "./Images/GreekFood4.jpg",
+]
+window.onload = () => {
+  document.body.style.backgroundImage = `url(${images[0]})`
+  document.querySelector('.hidden').src = images[1]
+  let i = 1
+  setInterval(() => {
+    document.body.style.backgroundImage = `url(${images[i++]})`
+
+    if (i === images.length) i = 0
+    else {
+      document.querySelector('.hidden').src = images[i]
+    }
+  }, 2000)
+}
